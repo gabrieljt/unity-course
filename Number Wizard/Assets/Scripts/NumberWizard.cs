cb2103ba470a66f4668275b01d695f736869fab4
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-01-11 23:38:28 -0200

    Lesson 18
    Scope and Variables

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -4,28 +4,40 @@ using System.Collections;
 public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
+	
 	void Start () {
 		print ("Welcome to Number Wizard!");
 		print ("Pick a number in your head, but don't tell me...");
+					
+		Debug.Log (string.Format("The highest number you can pick is {0}.", max));
+		Debug.Log (string.Format("The lowest number you can pick is {0}.", min));
 		
-		int max = 1000;
-		int min = 1;
+		// Round up
+		++max;
 		
-		print ("The highest number you can pick is " + max);
-		print ("The lowest number you can pick is " + min);
-		
-		print ("Is the number higher or lower than 500?");
-		print ("Up = higher, down = lower, return = equal.");
+		Guess();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			print ("Up arrow pressed");
+			min = guess;
+			guess = (max + min) / 2;
+			Guess();
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			print ("Down arrow pressed");
+			max = guess;
+			guess = (max + min) / 2;
+			Guess();
 		} else if (Input.GetKeyDown(KeyCode.Return)) {
 			print ("I won!");	
 		}
+	}
+	
+	void Guess () {
+		Debug.Log (string.Format("Is the number higher or lower than {0} ?", guess));
+		print ("up = higher | down = lower | return = equal");
 	}
 }
