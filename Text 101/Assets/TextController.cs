170d15b1ab1cf4d10969c35f5e165aafdf98e4d8
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-01-13 00:44:52 -0200

    Lesson 28
    Challenge 1

--- Text 101/Assets/TextController.cs
+++ Text 101/Assets/TextController.cs
@@ -14,6 +14,11 @@ public class TextController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space))
-			text.text = "Space key pressed!";
+			text.text = "Voce acorda num quarto escuro, aos poucos e com dificuldades. O corpo dolorido." +
+				" Apenas uma luz fraca de cor amarela entra por uma fresta na parede, mas e o suficiente" +
+				" para enxergar o contorno de alguns objetos no quarto. Nao se lembra de nada antes de acordar" +
+				" e com algum esforco consegue se lembrar do seu nome. Passados uns minutos, decide se levantar." +
+				" O quarto e pequeno. Alem da fresta na parede, voce avista um bau num canto do quarto, uma mesa" +
+				" e um espelho pendurado na parede. Nao parece haver uma porta...";
 	}
 }
