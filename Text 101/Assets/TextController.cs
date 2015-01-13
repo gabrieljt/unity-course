9ad4a6a691d226bcc32d4a8a3c06c5fd35f5a94b
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-01-13 00:23:13 -0200

    Lesson 27
    Challenge 1

--- Text 101/Assets/TextController.cs
+++ Text 101/Assets/TextController.cs
@@ -8,11 +8,12 @@ public class TextController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		text.text = "Hello world!";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown(KeyCode.Space))
+			text.text = "Space key pressed!";
 	}
 }
