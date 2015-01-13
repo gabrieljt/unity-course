b3a4c6e718c301453bdb8530c755c95bd37cf4ad
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-01-13 01:41:13 -0200

    Lesson 29/30
    Prison Script

--- Text 101/Assets/TextController.cs
+++ Text 101/Assets/TextController.cs
@@ -3,60 +3,87 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class TextController : MonoBehaviour {
-
 	public Text text;
-
-	private enum States {room, room_0, wall_crack_0, chest_0, table_0, mirror_0, freedom}
-	private States currentState;
-
+	private enum States {cell, sheets_0, sheets_1, lock_0, lock_1, mirror, cell_mirror, freedom};
+	private States myState;
 	// Use this for initialization
 	void Start () {
-		currentState = States.room;
+		myState = States.cell;
 	}
-	
 	// Update is called once per frame
 	void Update () {
-		switch (currentState) {
-		case States.room:
-			state_room();
-			break;
-		case States.room_0:
-			state_room_0();
-			break;
-		case States.wall_crack_0:
-			state_wall_crack_0();
-			break;
-		}
+		print (myState);
+		if (myState == States.cell) {state_cell();}
+		else if (myState == States.sheets_0) {state_sheets_0();}
+		else if (myState == States.sheets_1) {state_sheets_1();}
+		else if (myState == States.lock_0) {state_lock_0();}
+		else if (myState == States.lock_1) {state_lock_1();}
+		else if (myState == States.mirror) {state_mirror();}
+		else if (myState == States.cell_mirror) {state_cell_mirror();}
+		else if (myState == States.freedom) {state_freedom();}
 	}
 
-	void state_room () {
-		text.text = "Voce acorda num quarto escuro, aos poucos e com dificuldades. O corpo dolorido." +
-				" Apenas uma luz fraca de cor amarela entra por uma fresta na parede, mas e o suficiente" +
-				" para enxergar o contorno de alguns objetos no quarto. Nao se lembra de nada antes de acordar" +
-				" e com algum esforco consegue se lembrar do seu nome. Passados uns minutos, decide se levantar." +
-				" O quarto e pequeno. Alem da fresta na parede, voce avista um bau num canto do quarto, uma mesa" +
-				" e um espelho pendurado na parede. Nao parece haver uma porta...\n\n" +
-				" Pressione F para observar a Fresta; B para observar o Bau; M para observar a Mesa; E" +
-				" para observar o Espelho.";
-		if (Input.GetKeyDown(KeyCode.F))
-			currentState = States.wall_crack_0;
+	void state_cell() {
+		text.text = "You are in a prison cell, and you want to escape. There are " +
+				"some dirty sheets on the bed, a mirror on the wall, and the door " +
+				"is locked from the outside.\n\n" +
+				"Press S to view Sheets, M to view Mirror and L to view Lock" ;
+		if (Input.GetKeyDown(KeyCode.S)) {myState = States.sheets_0;}
+		else if (Input.GetKeyDown(KeyCode.M)) {myState = States.mirror;}
+		else if (Input.GetKeyDown(KeyCode.L)) {myState = States.lock_0;}
 	}
 
-	void state_room_0 () {
-		text.text = " Voce avista uma fresta na parede, um bau num canto do quarto, uma mesa" +
-				" e um espelho pendurado na parede. Nao parece haver uma porta...\n\n" +
-				" Pressione F para observar a Fresta; B para observar o Bau; M para observar a Mesa; E" +
-				" para observar o Espelho.";
-		if (Input.GetKeyDown(KeyCode.F))
-			currentState = States.wall_crack_0;
+	void state_mirror() {
+		text.text = "The dirty old mirror on the wall seems loose.\n\n" +
+				"Press T to Take the mirror, or R to Return to cell" ;
+		if (Input.GetKeyDown(KeyCode.T)) {myState = States.cell_mirror;}
+		else if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell;}
 	}
 
-	void state_wall_crack_0 () {
-		text.text = "A fresta nao e grande o suficiente para enxergar muita coisa do outro lado. Fora que" +
-				" a luz, apesar de fraca, ofusca sua visao. Mas nao parece ser um lugar aberto do outro lado desta" +
-				" parede...\n\n" +
-				" Pressione V para Voltar a observar o quarto.";
-		if (Input.GetKeyDown(KeyCode.V))
-			currentState = States.room_0;
+	void state_sheets_0() {
+		text.text = "You can't believe you sleep in these things. Surely it's " +
+				"time somebody changed them. The pleasures of prison life " +
+				"I guess!\n\n" +
+				"Press R to Return to roaming your cell" ;
+		if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell;}
+	}
+
+	void state_sheets_1() {
+		text.text = "Holding a mirror in your hand doesn't make the sheets look " +
+				"any better.\n\n" +
+				"Press R to Return to roaming your cell" ;
+		if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell_mirror;}
+	}
+
+	void state_lock_0() {
+		text.text = "This is one of those button locks. You have no idea what the " +
+				"combination is. You wish you could somehow see where the dirty " +
+				"fingerprints were, maybe that would help.\n\n" +
+				"Press R to Return to roaming your cell" ;
+		if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell;}
+	}
+
+	void state_lock_1() {
+		text.text = "You carefully put the mirror through the bars, and turn it round " +
+				"so you can see the lock. You can just make out fingerprints around " +
+				"the buttons. You press the dirty buttons, and hear a click.\n\n" +
+				"Press O to Open, or R to Return to your cell" ;
+		if (Input.GetKeyDown(KeyCode.O)) {myState = States.freedom;}
+		else if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell_mirror;}
+	}
+
+	void state_cell_mirror() {
+		text.text = "You are still in your cell, and you STILL want to escape! There are " +
+				"some dirty sheets on the bed, a mark where the mirror was, " +
+				"and that pesky door is still there, and firmly locked!\n\n" +
+				"Press S to view Sheets, or L to view Lock" ;
+		if (Input.GetKeyDown(KeyCode.S)) {myState = States.sheets_1;}
+		else if (Input.GetKeyDown(KeyCode.L)) {myState = States.lock_1;}
+	}
+
+	void state_freedom() {
+		text.text = "You are FREE!\n\n" +
+				"Press P to Play again";
+		if (Input.GetKeyDown(KeyCode.P)) {myState = States.cell;}
 	}
 }
