8e5a60b6dfe4080666cf0b91d0a6cb8c4f6deb6e
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-01-12 23:18:43 -0200

    Lesson 19
    Code Refactor

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -9,9 +9,13 @@ public class NumberWizard : MonoBehaviour {
 	int guess = 500;
 	
 	void Start () {
+		StartGame();
+	}
+
+	void StartGame () {
 		print ("Welcome to Number Wizard!");
 		print ("Pick a number in your head, but don't tell me...");
-					
+		
 		Debug.Log (string.Format("The highest number you can pick is {0}.", max));
 		Debug.Log (string.Format("The lowest number you can pick is {0}.", min));
 		
@@ -25,12 +29,10 @@ public class NumberWizard : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
 			min = guess;
-			guess = (max + min) / 2;
-			Guess();
+			NextGuess();
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
 			max = guess;
-			guess = (max + min) / 2;
-			Guess();
+			NextGuess();
 		} else if (Input.GetKeyDown(KeyCode.Return)) {
 			print ("I won!");	
 		}
@@ -39,5 +41,10 @@ public class NumberWizard : MonoBehaviour {
 	void Guess () {
 		Debug.Log (string.Format("Is the number higher or lower than {0} ?", guess));
 		print ("up = higher | down = lower | return = equal");
+	}
+
+	void NextGuess () {
+		guess = (max + min) / 2;
+		Guess();
 	}
 }
