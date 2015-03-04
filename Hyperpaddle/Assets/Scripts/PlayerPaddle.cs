3bc278eb5c666908c4eb7ac189a1cb61414e6357
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-04 17:25:09 -0300

    Lesson 243
    OnMouseDragged()

---
+++ Hyperpaddle/Assets/Scripts/PlayerPaddle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPaddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnMouseDrag () {
+		print ("Dragged to " + Input.mousePosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
