1c28ae2dcb3d5af3f54c0f8427667cb202b5e2a1
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-04 17:45:53 -0300

    Lesson 244
    worldUnitsFromCamera and screenPosition

--- Hyperpaddle/Assets/Scripts/PlayerPaddle.cs
+++ Hyperpaddle/Assets/Scripts/PlayerPaddle.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class PlayerPaddle : MonoBehaviour {
 
+	public Camera camera;
+	private float worldUnitsFromCamera;
+
 	// Use this for initialization
 	void Start () {
-	
+		worldUnitsFromCamera = transform.position.z - camera.transform.position.z;
 	}
 
 	void OnMouseDrag () {
-		print ("Dragged to " + Input.mousePosition);
+		Vector3 mousePosition = Input.mousePosition;
+		float mouseX = mousePosition.x;
+		float mouseY = mousePosition.y;
+		Vector3 screenPosition = new Vector3 (mouseX, mouseY, worldUnitsFromCamera);
+
+		print (screenPosition);
 	}
 	
 	// Update is called once per frame
