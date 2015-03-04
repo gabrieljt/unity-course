244d61e011bddf4c85d79c117be38f2fd3eb6c1e
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-04 18:16:47 -0300

    Lesson 246
    Code refactor.

--- Hyperpaddle/Assets/Scripts/PlayerPaddle.cs
+++ Hyperpaddle/Assets/Scripts/PlayerPaddle.cs
@@ -12,13 +12,23 @@ public class PlayerPaddle : MonoBehaviour {
 	}
 
 	void OnMouseDrag () {
+		Vector3 mousePosition = GetMouseCoordinatesAtDistance();
+		MovePaddleTo(mousePosition);
+	}
+
+	Vector3 GetMouseCoordinatesAtDistance () {
 		Vector3 mousePosition = Input.mousePosition;
 		float mouseX = mousePosition.x;
 		float mouseY = mousePosition.y;
-		Vector3 screenPosition = new Vector3 (mouseX, mouseY, worldUnitsFromCamera);
 
+		Vector3 screenPosition = new Vector3 (mouseX, mouseY, worldUnitsFromCamera);		
 		Vector3 worldPosition = camera.ScreenToWorldPoint(screenPosition);
-		transform.position = worldPosition;
+
+		return worldPosition;
+	}
+
+	void MovePaddleTo (Vector3 position) {
+		transform.position = position;
 	}
 	
 	// Update is called once per frame
