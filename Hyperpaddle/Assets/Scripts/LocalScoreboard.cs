a36c23952a1bb2a8966b0168f0d27dce4e830465
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-05 14:51:19 -0300

    Lesson 272
    Writing to file; cross platforms paths, Stream, FileStream, StreamWriter

---
+++ Hyperpaddle/Assets/Scripts/LocalScoreboard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class LocalScoreboard : MonoBehaviour {
+
+	void Start () {
+		string filePath = Path.Combine(Application.persistentDataPath, "hyperpaddle-scores.txt");
+		Debug.Log(filePath);
+		Stream file = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+		StreamWriter fileWriter = new StreamWriter(file);
+		fileWriter.Write("Hello World");
+		fileWriter.Close();
+	}
+	
+	void Update () {
+	
+	}
+}
