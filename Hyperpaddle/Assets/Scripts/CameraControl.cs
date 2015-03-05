688fd17148e2cd35c5e4dcf821b87919426cd9da
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-05 01:52:42 -0300

    Lesson 261
    Challenge accepted; moving camera but not shifting around paddle...

---
+++ Hyperpaddle/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public float factor;
+	private PlayerPaddle playerPaddle;
+
+	// Use this for initialization
+	void Start () {
+		playerPaddle = FindObjectOfType<PlayerPaddle>() as PlayerPaddle;
+		ShiftAroundPaddle(playerPaddle.transform.position);
+	}
+
+	void ShiftAroundPaddle (Vector3 paddlePosition) {
+		Vector3 moveTo;
+		
+		moveTo.x = Mathf.Clamp(paddlePosition.x, -7.5f, 7.5f);
+		moveTo.y = Mathf.Clamp(paddlePosition.y, -7.5f, 7.5f);
+		moveTo.z = transform.position.z;
+		
+		transform.position = moveTo;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ShiftAroundPaddle(playerPaddle.transform.position);	
+	}
+}
