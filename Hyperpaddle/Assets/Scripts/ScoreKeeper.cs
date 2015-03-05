4f48dbc45601bfcbced67c08cbc076afae54c100
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-05 00:28:32 -0300

    Lesson 258
    ScoreKeeper game object and script.

---
+++ Hyperpaddle/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour {
+
+	public Text scoreText;
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(gameObject);
+		scoreText.text = score.ToString();
+	}
+
+	void Add (int points) {
+		score += points;
+		scoreText.text = score.ToString();
+	}
+
+	int GetScore () {
+		return score;
+	}
+}
