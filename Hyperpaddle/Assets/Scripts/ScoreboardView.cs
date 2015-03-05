d0ddb74fb96ddb549b99d03c5feeb8ea1b8f6bf0
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-05 13:47:57 -0300

    Lesson 271
    Reverse().Take(5) and UI good looking text.

--- Hyperpaddle/Assets/Scripts/ScoreboardView.cs
+++ Hyperpaddle/Assets/Scripts/ScoreboardView.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -7,18 +8,22 @@ public class ScoreboardView : MonoBehaviour {
 
 	public Scoreboard scoreboard;
 	public Text scoresText;
+	public Text subtitle;
 
 	void Start () {
 		SortedList<int, string> scores = scoreboard.GetScores();
 
 		if (scores.Count > 0) {
-		string playerScores = "";
-		foreach(KeyValuePair<int, string> playerScore in scores) {
-			playerScores += string.Format("{0} <{1}>\n", playerScore.Value, playerScore.Key);
-		}
+			subtitle.text = (scores.Count < 5) ? string.Format("Top {0} Hyperpaddlers of all time!", scores.Count) : "Top 5 Hyperpaddlers of all time!";
 
-		scoresText.text = playerScores;
+			string playerScores = "";		
+			foreach(KeyValuePair<int, string> playerScore in scores.Reverse().Take(5)) {
+				playerScores += string.Format("{0} <{1}>\n", playerScore.Value, playerScore.Key);
+			}
+
+			scoresText.text = playerScores;
 		} else {
+			subtitle.text = "Best Hyperpaddlers of all time!";
 			scoresText.text = "Well... why don't you be the first?";
 		}
 	}
