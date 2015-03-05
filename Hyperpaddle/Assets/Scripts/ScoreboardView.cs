8667463a94105ad7bf4370257b6a80d95ccbe3b5
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-05 14:18:15 -0300

    Lesson 272
    Experimenting.

--- Hyperpaddle/Assets/Scripts/ScoreboardView.cs
+++ Hyperpaddle/Assets/Scripts/ScoreboardView.cs
@@ -8,14 +8,11 @@ public class ScoreboardView : MonoBehaviour {
 
 	public Scoreboard scoreboard;
 	public Text scoresText;
-	public Text subtitle;
 
 	void Start () {
 		SortedList<int, string> scores = scoreboard.GetScores();
-
+		
 		if (scores.Count > 0) {
-			subtitle.text = (scores.Count < 5) ? string.Format("Top {0} Hyperpaddlers of all time!", scores.Count) : "Top 5 Hyperpaddlers of all time!";
-
 			string playerScores = "";		
 			foreach(KeyValuePair<int, string> playerScore in scores.Reverse().Take(5)) {
 				playerScores += string.Format("{0} <{1}>\n", playerScore.Value, playerScore.Key);
@@ -23,7 +20,6 @@ public class ScoreboardView : MonoBehaviour {
 
 			scoresText.text = playerScores;
 		} else {
-			subtitle.text = "Best Hyperpaddlers of all time!";
 			scoresText.text = "Well... why don't you be the first?";
 		}
 	}
