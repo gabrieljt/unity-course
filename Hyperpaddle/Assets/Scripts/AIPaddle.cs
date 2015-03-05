dce32b7ef0ba68591cc943637d3add546b77ea7e
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-04 22:13:09 -0300

    Lesson 255
    Challenge accepted! AI Paddle script up and running :)

---
+++ Hyperpaddle/Assets/Scripts/AIPaddle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIPaddle : MonoBehaviour {
+
+	public GameObject ball;
+	private Vector3 ballPosition;
+
+	// Use this for initialization
+	void Start () {
+		MovePaddleToBallPosition2D();
+	}
+
+	void MovePaddleToBallPosition2D () {
+		ballPosition = ball.transform.position;	
+		ballPosition.z = transform.position.z;
+
+		Vector3 moveTo;
+		
+		moveTo.x = Mathf.Clamp(ballPosition.x, -7.5f, 7.5f);
+		moveTo.y = Mathf.Clamp(ballPosition.y, -7.5f, 7.5f);
+		moveTo.z = ballPosition.z;
+		
+		transform.position = moveTo;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MovePaddleToBallPosition2D();
+	}
+}
