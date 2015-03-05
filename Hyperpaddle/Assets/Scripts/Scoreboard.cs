d2cc641566992f1596710822368ca6b238ff8365
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-05 10:11:10 -0300

    Lesson 265
    Scoreboard API.

---
+++ Hyperpaddle/Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Scoreboard : MonoBehaviour {
+
+	void SetCurrentScore (int score) {
+	}
+
+	bool IsHighScore (int score) {
+		return true;
+	}
+
+	void ClaimCurrentScore (string name) {
+	}
+
+	SortedList<int, string> GetScores () {
+		return new SortedList<int, string>();
+	}
+}
