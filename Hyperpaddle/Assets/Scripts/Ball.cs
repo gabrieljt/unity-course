28ccf53539ca2df69bd12e53f18689997fe04fcc
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-04 21:24:57 -0300

    Lesson 254
    Challenge complete, I rock!!

--- Hyperpaddle/Assets/Scripts/Ball.cs
+++ Hyperpaddle/Assets/Scripts/Ball.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 
+	public Vector3 startVelocity;
+
 	// Use this for initialization
 	void Start () {
-	
+		startVelocity = new Vector3(0.0f, 0.0f, -40.0f);
+		rigidbody.velocity = startVelocity;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
