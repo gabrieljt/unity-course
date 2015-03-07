fb1b00d002bf512616cc602a90b8cd231b4565ec
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-07 20:14:07 -0300

    Marco Polo Tutorial
    PUN, monster prefab & PhotonView.

---
+++ Marco Polo/Assets/Scripts/RandomMatchmaker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMatchmaker : MonoBehaviour {
+
+	void Start () {
+		PhotonNetwork.logLevel = PhotonLogLevel.Full;
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+
+	void OnGUI () {
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnJoinedLobby() {
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed() {
+		Debug.Log("Could not connect to random room.");
+		PhotonNetwork.CreateRoom(null);
+	}
+}
