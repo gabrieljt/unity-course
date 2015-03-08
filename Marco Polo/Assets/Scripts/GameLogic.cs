fde70bbc61e6820819e05943a9ddfb64d188103c
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-07 23:00:26 -0300

    Marco Polo Tutorial
    GameLogic, Tagging Player via RPC.

--- Marco Polo/Assets/Scripts/GameLogic.cs
+++ Marco Polo/Assets/Scripts/GameLogic.cs
@@ -1,9 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameLogic : MonoBehaviour {
+public class GameLogic : Photon.MonoBehaviour {
 
 	public static int playerWhoIsIt;
+	private static PhotonView scenePhotonView;
+
+	void Start () {
+		scenePhotonView = GetComponent<PhotonView>();
+	}
+
+	void OnPhotonPlayerConnected (PhotonPlayer player) {
+		Debug.Log("OnPhotonPlayerConnected: " + player);
+
+		if (PhotonNetwork.isMasterClient)
+			TagPlayer(playerWhoIsIt);
+	}
 
 	void OnJoinedRoom () {
 		if (PhotonNetwork.playerList.Length == 1)
@@ -15,5 +27,10 @@ public class GameLogic : MonoBehaviour {
 	void TaggedPlayer (int playerID) {
 		playerWhoIsIt = playerID;
 		Debug.Log("TaggedPlayer: " + playerWhoIsIt);
+	}
+
+	public static void TagPlayer (int playerID) {
+		Debug.Log ("TagPlayer: " + playerID);
+		scenePhotonView.RPC("TaggedPlayer", PhotonTargets.All, playerID);
 	}
 }
