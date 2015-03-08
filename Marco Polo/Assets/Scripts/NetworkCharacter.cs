06401434d1c6e8790c26b06a5b9f4666fcf65628
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-07 21:30:34 -0300

    Marco Polo Tutorial
    Lerp over time.

--- Marco Polo/Assets/Scripts/NetworkCharacter.cs
+++ Marco Polo/Assets/Scripts/NetworkCharacter.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkCharacter : MonoBehaviour {
+public class NetworkCharacter : Photon.MonoBehaviour {
+
+	private Vector3 correctPlayerPosition;
+	private Quaternion corretPlayerRotation;
+
+	void Update () {
+		if (!photonView.isMine) {
+			transform.position = Vector3.Lerp(transform.position, correctPlayerPosition, Time.deltaTime * 5);
+			transform.rotation = Quaternion.Lerp(transform.rotation, corretPlayerRotation, Time.deltaTime * 5);
+		}
+	}
 
 	void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info) {
 		if (stream.isWriting) {
 			stream.SendNext(transform.position);
 			stream.SendNext(transform.rotation);
 		} else {
-			transform.position = (Vector3) stream.ReceiveNext();
-			transform.rotation = (Quaternion) stream.ReceiveNext();
+			correctPlayerPosition = (Vector3) stream.ReceiveNext();
+			corretPlayerRotation = (Quaternion) stream.ReceiveNext();
 		}
 	}
 }
