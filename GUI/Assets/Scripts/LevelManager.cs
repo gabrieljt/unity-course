59be8f0381edca5d829dadce587af0d6809ac8c3
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-19 23:49:40 -0300

    Lecture 49
    Application Class, LoadLevel() and self-challenge Quit().

--- GUI/Assets/Scripts/LevelManager.cs
+++ GUI/Assets/Scripts/LevelManager.cs
@@ -6,10 +6,12 @@ public class LevelManager : MonoBehaviour {
 	public void LoadLevel(string name)
 	{
 		Debug.Log("New Level load: " + name);
+		Application.LoadLevel(name);
 	}
 
 	public void QuitRequest()
 	{
 		Debug.Log("Quit Game requested.");
+		Application.Quit();
 	}
 }
