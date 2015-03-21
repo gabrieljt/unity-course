798da737ff908f19d226f879c1763783c798873b
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-21 18:34:37 -0300

    Lecture 75
    Play Space redone.

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;using System.Collections;public class Ball : MonoBehaviour {        public Paddle paddle;
     
-    bool launched = false;
-    Vector2 startVelocity;
-    float speedMultiplier = 1.03f;
+    private bool launched = false;
+    private Vector3 paddleToBallDistance;
+    private Vector2 startVelocity;
+    private float speedMultiplier = 1.03f;
 
     void Start()
     {
         startVelocity = new Vector2(Random.Range(-20f, 20f), Random.Range(10f, 20f));
+        paddleToBallDistance = transform.position - paddle.transform.position;
     }
 
     void Update()
@@ -14,8 +16,7 @@
         if (!launched)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
-            Vector3 paddlePosition = paddle.transform.position;
-            transform.position = new Vector3(paddlePosition.x, transform.position.y, 0f);
+            transform.position = paddle.transform.position + paddleToBallDistance;
 
             if (Input.GetMouseButtonDown(0))
             {
