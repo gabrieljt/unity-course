c951475b4094b399f0e74f93d3e7101e49c4fcd4
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-21 14:08:52 -0300

    Lecture 68
    Play Space

--- Block Breaker/Assets/Scripts/MusicPlayer.cs
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -1,21 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class MusicPlayer : MonoBehaviour {
-
-    static MusicPlayer instance = null;
-
-	void Awake() 
-	{
-        if (instance != null)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            instance = this;
-            GameObject.DontDestroyOnLoad(gameObject);
-        }
-	}
-	
-}
+﻿using UnityEngine;using System.Collections;public class MusicPlayer : MonoBehaviour {    static MusicPlayer instance = null;	void Awake() 	{        if (instance != null)        {            Destroy(gameObject);        }        else        {            instance = this;            GameObject.DontDestroyOnLoad(gameObject);        }	}	}
