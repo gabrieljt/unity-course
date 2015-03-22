01621e4183fa77e326db39287cc9139a2836dfb1
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-22 01:41:13 -0300

    Lecture 88
    Self-challenge: FollowBall.

--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -1,9 +1,33 @@
 ﻿using UnityEngine;using System.Collections;public class Paddle : MonoBehaviour {
 
+    public bool debug;
+
     void Start()
     {
         Cursor.visible = false;
-    }	void Update()     {        float mousePositionInWorldUnits = Input.mousePosition.x / Screen.width * 16;        Vector3 position = new Vector3(0f, transform.position.y, 0f);        position.x = Mathf.Clamp(mousePositionInWorldUnits, 0.5f, 15.5f);        transform.position = position;	}
+    }	void Update()     {
+        if (!debug)
+        {
+            FollowMouse();
+        }
+        else
+        {
+            FollowBall();
+        }        	}
+
+    void FollowMouse()
+    {
+        float mousePositionInWorldUnits = Input.mousePosition.x / Screen.width * 16;
+        Vector3 position = new Vector3(0f, transform.position.y, 0f);
+        position.x = Mathf.Clamp(mousePositionInWorldUnits, 0.5f, 15.5f);
+        transform.position = position;
+    }
+
+    void FollowBall()
+    {
+        Vector3 ballPosition = GameObject.FindObjectOfType<Ball>().transform.position;
+        transform.position = new Vector3(Mathf.Clamp(ballPosition.x, 0.5f, 15.5f), transform.position.y, transform.position.z);
+    }
 
     void OnDestroy()
     {
