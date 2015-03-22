eb6adef79ec2d905f81fb321c2de4354bfb2cd1b
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-22 16:40:11 -0300

    Lecture 101
    Self-challenge: PlayerController, Mathf.Clamp.

---
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float velocity = 0.15f;
+
+	void Update () 
+	{
+		HandleInput();
+	}
+
+	void HandleInput()
+	{
+		Vector3 position = transform.position;
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			position.x -= velocity;
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			position.x += velocity;
+		}
+
+		transform.position = new Vector3(Mathf.Clamp(position.x, -6.15f, 6.15f), position.y, position.z);
+	}
+
+}
