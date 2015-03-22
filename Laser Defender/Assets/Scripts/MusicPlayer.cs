46b5df23887443040b45660b29b18db68e5119a0
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-22 15:32:56 -0300

    Lecture 100
    Laser Defender first steps.

---
+++ Laser Defender/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+	
+	void Start () 
+	{
+		if (instance != null && instance != this) 
+		{
+			Destroy(gameObject);
+		} 
+		else 
+		{
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}		
+	}
+
+}
