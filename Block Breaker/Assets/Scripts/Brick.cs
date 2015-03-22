fc5d03b9282c21c0dae9aa4468ddd86ecfc4ea25
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-22 00:03:53 -0300

    Lesson 84
    Breakable tag.

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -8,17 +8,31 @@ public class Brick : MonoBehaviour {
 
     private LevelManager levelManager;
     private static int bricksCount;
-    private int maxHits;
+    private int maxHits = 0;
     private int timesHit = 0;
+    bool isBreakable;
 
 	void Start()    
     {
         levelManager = GameObject.FindObjectOfType<LevelManager>();
-        bricksCount = GameObject.FindObjectsOfType<Brick>().Length;
-        maxHits = hitSprites.Length + 1;
+        isBreakable = tag.Equals("Breakable");
+
+        if (isBreakable)
+        {
+            bricksCount = GameObject.FindGameObjectsWithTag("Breakable").Length;
+            maxHits = hitSprites.Length + 1;
+        }        
 	}
 	
     void OnCollisionEnter2D()
+    {
+        if (isBreakable)
+        {
+            HandleHits();
+        }
+    }
+
+    void HandleHits()
     {
         timesHit++;
 
@@ -41,7 +55,10 @@ public class Brick : MonoBehaviour {
     public void LoadSprites()
     {
         int spriteIndex = timesHit - 1;
-        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        if (hitSprites[spriteIndex])
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
     }
 
 }
