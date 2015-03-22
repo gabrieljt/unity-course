faaf5d3bcbaba80a82712b7e5bfd03d4c2455607
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-21 23:11:49 -0300

    Lecture 82
    Level 1 redesigned, self-challenge: cursor visable true and false.

--- Block Breaker/Assets/Scripts/Paddle.cs
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -1,1 +1,11 @@
-﻿using UnityEngine;using System.Collections;public class Paddle : MonoBehaviour {    	void Update()     {        float mousePositionInWorldUnits = Input.mousePosition.x / Screen.width * 16;        Vector3 position = new Vector3(0f, transform.position.y, 0f);        position.x = Mathf.Clamp(mousePositionInWorldUnits, 0.5f, 15.5f);        transform.position = position;	}}
+﻿using UnityEngine;using System.Collections;public class Paddle : MonoBehaviour {
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }	void Update()     {        float mousePositionInWorldUnits = Input.mousePosition.x / Screen.width * 16;        Vector3 position = new Vector3(0f, transform.position.y, 0f);        position.x = Mathf.Clamp(mousePositionInWorldUnits, 0.5f, 15.5f);        transform.position = position;	}
+
+    void OnDestroy()
+    {
+        Cursor.visible = true;
+    }}
