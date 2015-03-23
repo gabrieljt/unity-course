0c4576660bf92923cb524c741472d2d0f6fb2cde
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-22 22:58:41 -0300

    Lecture 106
    Self-challenge: destroying lasers outside world bounds.

--- Laser Defender/Assets/Entities/Projectiles/Laser.cs
+++ Laser Defender/Assets/Entities/Projectiles/Laser.cs
@@ -6,8 +6,27 @@ public class Laser : MonoBehaviour {
 	public float velocity = 5f;
 	public int direction = 1;
 
+	private float bottomBoundary, topBoundary;
+
+	void Start()
+	{
+		SetBounds();
+	}
+
 	void Update() 
 	{
 		transform.position += new Vector3(0f, direction * velocity * Time.deltaTime, 0f);
+
+		if (transform.position.y < bottomBoundary || transform.position.y > topBoundary)
+			Destroy(gameObject);
+	}
+
+	void SetBounds()
+	{
+		Camera camera = Camera.main;
+		float laserToCameraDistance = transform.position.z - camera.transform.position.z;
+		
+		bottomBoundary = camera.ViewportToWorldPoint(new Vector3(0f, 0f, laserToCameraDistance)).y;
+		topBoundary = camera.ViewportToWorldPoint(new Vector3(1f, 1f, laserToCameraDistance)).y;
 	}
 }
