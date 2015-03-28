5e73e43b44443042045ebe17935cf6a9c3960fd2
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-03-28 18:35:48 -0300

    uLink
    Adding players avatar

---
+++ uLink/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+
+	void OnDrawGizmos ()
+	{
+		Gizmos.DrawCube (transform.position, new Vector3 (1f, 1f, 1f));
+	}
+
+}
